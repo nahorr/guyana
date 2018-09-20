fdfdca40907fd0955112a7df9c84cdfcf6b2b229
nahorr <nahorr@gmail.com>
2018-09-20 10:01:09 -0600

    edit verify email

--- resources/views/emails/verifyUser.blade.php
+++ resources/views/emails/verifyUser.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Welcome to Gosgem</title>
+	<title>Welcome to Guyana Localcontent 2018</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
 
-	<h4>Welcome to Gosgem!</h4>
+	<h4>Welcome to Guyana Localcontent 2018!</h4>
 	<br/>
 		<p>Hi {{$user->name}},</p>
 		<p>Please click on the below link to verify your email account.</p>	
@@ -14,7 +14,7 @@
 	<br/>
 		<a href="{{url('user/verify', $user->verifyUser->token)}}"><button class="button-save large" style="background-color: #b5d100;">Verify Email</button></a>
 	<br/>
-		<p>Thank you - from Gosgem</p>
+		<p>Thank you - from https://guyana.localcontentworld.com</p>
 
 </body>
 </html>
